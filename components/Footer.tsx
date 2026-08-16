5fe9ae8c6282f6c78c2b6ff383b56d67c4152744
Vitor Brandao <vbrandao@inovvati.com.br>
2026-08-16 18:41:32 -0400

    feat: adiciona componente Footer com logo e botão de voltar ao topo

---
+++ components/Footer.tsx
@@ -0,0 +1,26 @@
+import { ArrowUpIcon } from "lucide-react";
+
+export const Footer = () => {
+  return (
+    <footer className="flex items-center justify-center min-h-20 bg-primary mx-auto w-screen">
+      <div className="flex gap-4 items-center justify-between max-w-6xl mx-auto px-6 w-full">
+        <img
+          src="/logos/logo-baixo-branca.png"
+          alt="Click Lavanderia Logo"
+          className="h-12 w-auto"
+        />
+
+        <p className="text-xs text-white/80 text-center">
+          © 2023 Click Lavanderia. Todos os direitos reservados.
+        </p>
+
+        <a
+          href="#top"
+          className="flex items-center justify-center bg-white rounded-full min-h-8 min-w-8 text-primary"
+        >
+          <ArrowUpIcon size={20} />
+        </a>
+      </div>
+    </footer>
+  );
+};
